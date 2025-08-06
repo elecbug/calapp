fa306e95a74baa53ed1aef4aa317337ca380c20a
elecbug <deveb1479@gmail.com>
2025-08-06 10:09:11 +0900

    Add project files.

---
+++ Calapp/App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calapp
+{
+    internal class App
+    {
+        private static App? _instance;
+
+        public static App Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new App();
+                }
+
+                return _instance;
+            }
+        }
+
+        private App()
+        {
+
+        }
+    }
+}
